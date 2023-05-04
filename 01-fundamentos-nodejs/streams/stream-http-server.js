54fff676d6c2ba2524ff45b68fa662604183ccfc
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2023-05-04 12:49:24 -0300

    aplicando streams no módulo HTTP

---
+++ 01-fundamentos-nodejs/streams/stream-http-server.js
@@ -0,0 +1,16 @@
+import http from 'node:http';
+import { Transform } from 'node:stream';
+
+class InverseNumber extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1;
+    console.log(transformed);
+    callback(null, String(transformed));
+  }
+}
+
+const server = http.createServer((req, res) => {
+  return req.pipe(new InverseNumber()).pipe(res);
+});
+
+server.listen(3334);
